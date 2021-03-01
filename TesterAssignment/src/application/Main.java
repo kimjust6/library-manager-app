8d96b79beb41971adfe7c10ba4a29b850522f96c
Justin Kim <mletemps@hotmail.com>
2021-03-01 13:32:14 -0500

    Added tests to see if push will work

--- TesterAssignment/src/application/Main.java
+++ TesterAssignment/src/application/Main.java
@@ -21,6 +21,9 @@ public class Main extends Application {
 	}
 	
 	public static void main(String[] args) {
+		//This is a test by Justin Kim
+		console.log("Test by Justin to see if Push works");
+		
 		launch(args);
 	}
 }
