148e60a434fc2af5da9e5b5a4c88af8b28b0f1c6
Harsh K <hkmkeswani@myseneca.ca>
2021-03-02 11:11:47 -0500

    added fx15 classpath

--- TesterAssignment/src/application/Main.java
+++ TesterAssignment/src/application/Main.java
@@ -1,7 +1,6 @@
 package application;
 	
 import javafx.application.Application;
-import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
