95bd07b3bd4d52c6c82fe4dfd326f0a056638a14
Harsh K <hkmkeswani@myseneca.ca>
2021-03-03 10:59:57 -0500

    linking pages

--- TesterAssignment/src/application/Admin_Login_Page.java
+++ TesterAssignment/src/application/Admin_Login_Page.java
@@ -1,7 +1,16 @@
 package application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 
 public class Admin_Login_Page implements EventHandler<ActionEvent>{
@@ -9,12 +18,43 @@ public class Admin_Login_Page implements EventHandler<ActionEvent>{
 	@Override
 	public void handle(ActionEvent arg0) {
 		// TODO Auto-generated method stub
-		System.out.println("Admin button is clicked");
-		GridPane pane = new GridPane();
-		pane.setAlignment(Pos.CENTER);
-		
-		 pane.setHgap(5.5);
-		 pane.setVgap(10);
+		try {
+			BorderPane rootPane = new BorderPane();
+			ImageView imageview = new ImageView(new Image("https://ih1.redbubble.net/image.1361936297.3666/tst,small,507x507-pad,600x600,f8f8f8.jpg"));
+			imageview.setFitHeight(250);
+			imageview.setFitWidth(250);
+			
+			GridPane loginPane = new GridPane();
+			loginPane.setAlignment(Pos.CENTER);
+			loginPane.setPadding(new Insets(11.5, 12.5, 13.5, 14.5));
+			loginPane.setHgap(5.5);
+			loginPane.setVgap(5.5);
+			
+			loginPane.add(new Label("Username:"), 0, 1);
+			loginPane.add(new TextField(), 1, 1);
+			loginPane.add(new Label("Password:"), 0, 2);
+			loginPane.add(new PasswordField(), 1, 2);
+			
+			Button btn = new Button("Login");
+			btn.setOnAction(e -> {
+				System.out.println("You tried to login.");
+			});
+			loginPane.add(btn, 1, 3);
+			
+			rootPane.setTop(imageview);
+			rootPane.setCenter(loginPane);
+			Scene scene = new Scene(rootPane);
+			
+			primaryStage.setResizable(false);
+			primaryStage.setTitle("brr");
+		    primaryStage.setScene(scene);
+			primaryStage.show();
+			
+			// something
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
