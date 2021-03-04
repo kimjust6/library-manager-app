6efd27e6e2c7b7a2b5727ebe2a5c8a3b166f27bc
Justin Kim <mletemps@hotmail.com>
2021-03-03 22:30:18 -0500

    added functionality to checkout and return book, but might have broken update

--- TesterAssignment/src/DatabaseTest/Main.java
+++ TesterAssignment/src/DatabaseTest/Main.java
@@ -9,23 +9,28 @@ public class Main {
     	postgreSQLHeroku db = new postgreSQLHeroku();
 
         
-        db.create_table(db.TABLE_STUDENTS, "sid SERIAL", "firstname varchar(50)", "lastname varchar(50)");
-        db.create_table("users", "id SERIAL", "username varchar(50)", "password varchar(50)", "userType varchar(50)");
-        db.insert(db.TABLE_STUDENTS,"3","draco","malfoy");
-        db.insert(db.TABLE_STUDENTS,"1","harry","potter");
-        db.insert(db.TABLE_STUDENTS,"2","ron","weasley");
-        db.insert("users","2","gaben","mypassword",db.TYPE_ADMIN);
-        db.update(db.TABLE_STUDENTS, "sid", "1", "lastname", "anewlastname");
-        db.update(db.TABLE_STUDENTS, "sid", "1", "lastname", "anewerlastname");
+//        db.create_table(db.TABLE_STUDENTS, "sid SERIAL", "firstname varchar(50)", "lastname varchar(50)");
+//        db.create_table("users", "id SERIAL", "username varchar(50)", "password varchar(50)", "userType varchar(50)");
+//        db.insert(db.TABLE_STUDENTS,"3","draco","malfoy");
+//        db.insert(db.TABLE_STUDENTS,"1","harry","potter");
+//        db.insert(db.TABLE_STUDENTS,"2","ron","weasley");
+//        db.insert("users","2","gaben","mypassword",db.TYPE_ADMIN);
+//        db.update(db.TABLE_STUDENTS, "sid", "1", "lastname", "anewlastname");
+//        db.update(db.TABLE_STUDENTS, "sid", "1", "lastname", "anewerlastname");
         
-        db.selectAll("students","firstname");
-        db.delete("students","sid","1");
+//        db.select_all("students","firstname");
+//        db.delete("students","sid","1");
         
 //        db.insert(conn, db.tableUsers, "johnsnow", "myverysecurepassword", "Librarian");
 //        db.insert(conn, "admins", "harryPotter", "myverysecurepassword", "Admin");
 //        db.select(conn,"admins");
 //        db.update(conn,"admins","harryPotter","myverysecurepassword3");
 //        db.select(conn,"admins");
+    	
+//    	db.insert_library("harry potter","j.k. rowling","a publisher", db.TYPE_BOOK, "20");
+    	//db.checkout_lib("1");
+    	db.return_lib("1");
+    	
         
     }
 }
