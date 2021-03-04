c0d233a0a0f354277ea9232fdf0f5badbf7b5286
Harsh K <hkmkeswani@myseneca.ca>
2021-03-04 15:18:16 -0500

    split up all scenes into classes

--- TesterAssignment/src/DatabaseTest/postgreSQLHeroku.java
+++ TesterAssignment/src/DatabaseTest/postgreSQLHeroku.java
@@ -7,57 +7,55 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 
-
-
-public class postgreSQLHeroku{
+public class postgreSQLHeroku {
 
 	//final strings to connect to the database
-    public final String DATABASE_URL = "jdbc:postgresql://ec2-54-209-43-223.compute-1.amazonaws.com:5432/d19rc88931g1bi";
-    public final String DATABASE_USERNAME = "luwrnpzmqrzvln";
-    public final String DATABASE_PASSWORD = "9b76f4cfa5a87feb4cf28e8b90e485b183bca39b2b20c09e323b4a04b524b2ce";
+    public static final String DATABASE_URL = "jdbc:postgresql://ec2-54-209-43-223.compute-1.amazonaws.com:5432/d19rc88931g1bi";
+    public static final String DATABASE_USERNAME = "luwrnpzmqrzvln";
+    public static final String DATABASE_PASSWORD = "9b76f4cfa5a87feb4cf28e8b90e485b183bca39b2b20c09e323b4a04b524b2ce";
     
     //final strings for table names 
-    public final String TABLE_ADMINS = "admins";
-    public final String TABLE_USERS = "users";
-    public final String TABLE_STUDENTS = "students";
-    public final String TABLE_LIBRARY = "library";
-    public final String TABLE_BORROWED_OBJECTS = "borrowedobjects";
-    public final String TABLE_WAITLIST_OBJECTS = "waitlistobjects";
+    public static final String TABLE_ADMINS = "admins";
+    public static final String TABLE_USERS = "users";
+    public static final String TABLE_STUDENTS = "students";
+    public static final String TABLE_LIBRARY = "library";
+    public static final String TABLE_BORROWED_OBJECTS = "borrowedobjects";
+    public static final String TABLE_WAITLIST_OBJECTS = "waitlistobjects";
     
     //final strings for column names in table user
     
     //final strings for column names in table user
-    public final String COL_USERNAME = "username"; 
-    public final String COL_PASSWORD = "password"; 
-    public final String COL_ADMINTYPE = "admintype";
+    public static final String COL_USERNAME = "username"; 
+    public static final String COL_PASSWORD = "password"; 
+    public static final String COL_ADMINTYPE = "admintype";
     
     //final strings for user types
-    public final String TYPE_ADMIN = "admin";
-    public final String TYPE_MANAGER = "manager";
-    public final String TYPE_LIBRARIAN = "librarian";
+    public static final String TYPE_ADMIN = "admin";
+    public static final String TYPE_MANAGER = "manager";
+    public static final String TYPE_LIBRARIAN = "librarian";
     
     //final strings for column names in table library (book)    
-    public final String COL_TITLE = "title";
-    public final String COL_QTY_AVAIL = "qtyAvailable";
-    public final String COL_QTY_BOR = "qtyBorrowed";
-    public final String COL_TYPE = "type";
-    public final String COL_PUBLISHER = "publisher";
-    public final String COL_ID = "libid";
+    public static final String COL_TITLE = "title";
+    public static final String COL_QTY_AVAIL = "qtyAvailable";
+    public static final String COL_QTY_BOR = "qtyBorrowed";
+    public static final String COL_TYPE = "type";
+    public static final String COL_PUBLISHER = "publisher";
+    public static final String COL_ID = "libid";
     
     //final strings for library (book) types
-    public final String TYPE_BOOK = "book";
-    public final String TYPE_MAG = "magazine";
-    public final String TYPE_VID = "video";
+    public static final String TYPE_BOOK = "book";
+    public static final String TYPE_MAG = "magazine";
+    public static final String TYPE_VID = "video";
     
     //final strings for column names in table student
-    public final String COL_STUD_NO = "studentno";
-    public final String COL_STUD_LVL = "studentLvl";
+    public static final String COL_STUD_NO = "studentno";
+    public static final String COL_STUD_LVL = "studentLvl";
     
     //final strings for student levels
-    public final String TYPE_ALUM = "alumni";
-    public final String TYPE_CURR = "current";
-    public final String TYPE_UNDER = "undergrad";
-    public final String TYPE_GRAD = "graduate";
+    public static final String TYPE_ALUM = "alumni";
+    public static final String TYPE_CURR = "current";
+    public static final String TYPE_UNDER = "undergrad";
+    public static final String TYPE_GRAD = "graduate";
     
     //private variable that connects to database
     private Connection m_conn = null;
@@ -65,7 +63,7 @@ public class postgreSQLHeroku{
     //constructor
     public postgreSQLHeroku()
     {
-    	connect();
+//    	connect();
     }
     
     
@@ -102,39 +100,6 @@ public class postgreSQLHeroku{
 
         return;
     }
-
-
-    // temporary method - Harsh K
-    
-    public ResultSet initialize(String username, String password) {
-    	String query = "";
-    	ResultSet queryResult = null;
-    	try {
-        	Statement statement = this.m_conn.createStatement();
-        	
-    		query = "select * from " + TABLE_USERS + " where " + COL_USERNAME + "='" + username + "';";
-    		queryResult = statement.executeQuery(query); 
-    		
-    		if(queryResult.next()) {
-
-    			if (queryResult.getString(this.COL_PASSWORD).equals(password)) { 
-    				ResultSet userInfo = statement.executeQuery("select * from " + TABLE_ADMINS + " where " + COL_USERNAME + "='" + username + "';");
-    				
-    				if(userInfo.next()) return userInfo;
-    				
-    			} else {
-    				System.out.println("Invalid password!");
-    			}
-    		} else {
-    			System.out.println("User does not exist!");
-    		}
-    	} catch (Exception e) {
-    		e.printStackTrace();
-    	}
-		return null;
-    }
-    
-     
     
     public boolean insert_librarian(String tableName, String username, String password, String usertype, String onHandQty, String borrowedQty)
     {
---
+++ TesterAssignment/src/application/AdminLogin.java
@@ -0,0 +1,108 @@
+package application;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+import DatabaseTest.postgreSQLHeroku;
+
+public class AdminLogin implements AutoCloseable {
+	
+	private Stage stage;
+	private Scene scene;
+	
+	public AdminLogin(Stage stage, Scene scene) {
+		this.stage = stage;
+		this.scene = scene;
+	}
+	
+	public Scene showLoginPage() throws Exception {
+		GridPane pane = new GridPane();
+		pane.setAlignment(Pos.CENTER);
+		pane.setPadding(new Insets(11.5, 12.5, 13.5, 14.5));
+		pane.setHgap(5.5);
+		pane.setVgap(5.5);
+			
+		TextField username = new TextField();
+		PasswordField password = new PasswordField();
+		
+		pane.add(new Label("Username:"), 0, 1);
+		pane.add(username, 1, 1);
+		pane.add(new Label("Password:"), 0, 2);
+		pane.add(password, 1, 2);
+			
+		Button btn = new Button("Login");
+		btn.setOnAction(new EventHandler<ActionEvent>() {
+			@Override public void handle(ActionEvent arg0) {
+				try (Connection connection = DriverManager.getConnection(postgreSQLHeroku.DATABASE_URL, postgreSQLHeroku.DATABASE_USERNAME, postgreSQLHeroku.DATABASE_PASSWORD)) {
+					
+					Statement statement = connection.createStatement();
+					String query = "select * from " + postgreSQLHeroku.TABLE_USERS + " where " + postgreSQLHeroku.COL_USERNAME + "='" + username.getText() + "';";
+										
+					ResultSet queryResult = statement.executeQuery(query); 
+					
+					if(queryResult.next()) {
+		    			if (queryResult.getString(postgreSQLHeroku.COL_PASSWORD).equals(password.getText())) { 
+		    				String query2 = "select * from " + postgreSQLHeroku.TABLE_ADMINS + " where " + postgreSQLHeroku.COL_USERNAME + "='" + username.getText() + "';";
+		    				ResultSet userInfo = statement.executeQuery(query2);
+		    				
+		    				if(userInfo.next()) {
+		    					String userType = userInfo.getString(postgreSQLHeroku.COL_ADMINTYPE);
+								if(userType.equalsIgnoreCase("Admin")) {
+									stage.setTitle("Admin Menu");
+									try (AdminMenu adminMenu = new AdminMenu(stage, scene)) {
+										stage.setScene(adminMenu.showMenu());
+									} catch (Exception e) {
+										e.printStackTrace();
+									}
+									
+								} else if(userType.equalsIgnoreCase("Librarian")) {
+									stage.setTitle("Librarian Menu");
+									try (LibrarianMenu librarianMenu = new LibrarianMenu(stage, scene)) {
+										stage.setScene(librarianMenu.showMenu());
+									} catch (Exception e) {
+										e.printStackTrace();
+									}
+									
+								} else {
+									System.out.println("You are not an admin or librarian!");
+								}
+		    				}
+		    			} else {
+		    				System.out.println("Invalid password!");
+		    			}
+		    		} else {
+		    			System.out.println("User does not exist!");
+		    		}
+				} catch (SQLException e1) {
+					e1.printStackTrace();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		pane.add(btn, 1, 3);
+		
+		Scene scene = new Scene(pane, 350, 450);
+	    scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+	    return scene;
+	}
+
+	@Override
+	public void close() throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+}
