bafc47d6f3bfcb0bd5c578ca28722d2cbe0fd4a2
Harsh K <hkmkeswani@myseneca.ca>
2021-03-04 02:53:00 -0500

    added more admin functionality rev3

--- TesterAssignment/src/application/Main.java
+++ TesterAssignment/src/application/Main.java
@@ -126,12 +126,12 @@ public class Main extends Application {
 						if(userType.equalsIgnoreCase("Admin")) {
 							System.out.println("You are an Admin!");
 							stage.setScene(adminMenu());
-							stage.setTitle("Admin Page");
+							stage.setTitle("Admin Menu");
 							
 						} else if(userType.equalsIgnoreCase("Librarian")) {
 							System.out.println("You are a librarian!");
-//							stage.setScene(librarianHomePage());
-//							stage.setTitle("Librarian Page");
+							stage.setScene(librarianMenu());
+							stage.setTitle("Librarian Menu");
 							
 						} else {
 							System.out.println("You are not an admin or librarian!");
@@ -219,6 +219,74 @@ public class Main extends Application {
 		pane.add(imgV2, 0, 2);
 		pane.add(viewBtn, 1, 2);
 		
+		Scene scene = new Scene(pane, 350, 450);
+		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		return scene;
+	}
+	
+	
+	// if ADMIN is LOGGED IN, show MENU OPTIONS
+	
+	public Scene librarianMenu() throws Exception {
+		GridPane pane = new GridPane();
+		pane.setAlignment(Pos.CENTER);
+		pane.setHgap(10);
+		pane.setVgap(20);
+		    
+		Button addBtn = new Button("Add Book");
+		Button viewBtn = new Button("View Books");
+		Button issueBtn = new Button("Issue Book");
+		Button returnBtn = new Button("Return Book");
+			
+		addBtn.setOnAction(new EventHandler<ActionEvent>(){
+			@Override public void handle(ActionEvent arg0) {
+				try (Scanner in = new Scanner(System.in)) {
+//					stage.setScene(pagegoeshere);
+					stage.setTitle("Adding Book");
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+			
+		viewBtn.setOnAction(new EventHandler<ActionEvent>(){
+			@Override public void handle(ActionEvent arg0) {
+				try (Scanner in = new Scanner(System.in)) {
+//					stage.setScene(pagegoeshere);
+					stage.setTitle("Viewing Books");
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+			
+		issueBtn.setOnAction(new EventHandler<ActionEvent>(){
+			@Override public void handle(ActionEvent arg0) {
+				try (Scanner in = new Scanner(System.in)) {
+//					stage.setScene(pagegoeshere);
+					stage.setTitle("Issuing Book");
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		
+		returnBtn.setOnAction(new EventHandler<ActionEvent>(){
+			@Override public void handle(ActionEvent arg0) {
+				try (Scanner in = new Scanner(System.in)) {
+//					stage.setScene(pagegoeshere);
+					stage.setTitle("Returning Book");
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+			
+		pane.add(addBtn, 0, 0);
+		pane.add(viewBtn, 0, 1);
+		pane.add(issueBtn, 0, 2);
+		pane.add(returnBtn, 0, 3);
+			
 		Scene scene = new Scene(pane, 350, 450);
 		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 		return scene;
