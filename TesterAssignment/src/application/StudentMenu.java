71081bad7b2769d99a5bf00b7c3068a4c27a9392
Justin Kim <mletemps@hotmail.com>
2021-03-05 13:41:49 -0500

    removed an old button that wasn't being used.

--- TesterAssignment/src/application/StudentMenu.java
+++ TesterAssignment/src/application/StudentMenu.java
@@ -1,5 +1,14 @@
 package application;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.postgresql.util.PSQLException;
+
+import DatabaseTest.postgreSQLHeroku;
 import classes.Student;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -30,12 +39,11 @@ public class StudentMenu implements AutoCloseable {
       	Label heading = new Label("Welcome, " + stud.getfName() + "\nWhat would you like to do today?\n");
       	
       	Button searchBtn = new Button("Find Books");
-      	Button reqIssueBtn = new Button("Request a book");
-      	Button viewBorrowedBtn = new Button("View Borrowed");
+      	Button viewBorrowedBtn = new Button("View Borrowed Items");
       	Button backBtn = new Button("Logout");
       	
       	searchBtn.setMaxWidth(WIDTH);
-      	reqIssueBtn.setMaxWidth(WIDTH);
+
       	viewBorrowedBtn.setMaxWidth(WIDTH);
       	backBtn.setMaxWidth(WIDTH);
       	
@@ -51,27 +59,54 @@ public class StudentMenu implements AutoCloseable {
 			}
       	});
       	
-      	reqIssueBtn.setOnAction(new EventHandler<ActionEvent>(){
+      	viewBorrowedBtn.setOnAction(new EventHandler<ActionEvent>(){
 			@Override public void handle(ActionEvent arg0) {
-				try {
-//					stage.setScene(pagegoeshere);
-					stage.setTitle("Book Issue Request");
+				try(Connection connection = DriverManager.getConnection(postgreSQLHeroku.DATABASE_URL, postgreSQLHeroku.DATABASE_USERNAME, postgreSQLHeroku.DATABASE_PASSWORD)) {
+
+					Statement statement = connection.createStatement();
+					String query = "";
+					
+					
+					query = String.format("select s.studentno, s.fname, s.lname, lib.libid, lib.title, lib.author, lib.publisher, lib.media_type from students s "
+							+ "join borrowedobjects bo on (s.studentno = bo.studentno) join library lib on (bo.libid = lib.libid) where %s='%s';",
+							postgreSQLHeroku.COL_STUD_NO,stud.getStudentNo());
+					query = String.format("select * from %s where %s = %s;", postgreSQLHeroku.TABLE_BORROWED_OBJECTS, postgreSQLHeroku.COL_USERNAME, stud.getStudentNo());
+					
+
+					System.out.println(query);
+					
+					ResultSet queryResult = statement.executeQuery(query); 
+					
+					
+					try (BorrowedLibTable borrowTable = new BorrowedLibTable(stage, scene)) 
+					{
+						stage.setScene(borrowTable.showMenu(queryResult, stud));
+						stage.setTitle("Search Results");
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+					
+
+				} catch (PSQLException e2) {
+					e2.printStackTrace();
+				} catch (SQLException e1) {
+					e1.printStackTrace();
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
       	});
       	
-      	viewBorrowedBtn.setOnAction(new EventHandler<ActionEvent>(){
-			@Override public void handle(ActionEvent arg0) {
-				try {
-//					stage.setScene(pagegoeshere);
-					stage.setTitle("Borrowed");
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-      	});
+//      	viewBorrowedBtn.setOnAction(new EventHandler<ActionEvent>(){
+//			@Override public void handle(ActionEvent arg0) {
+//				try {
+////					stage.setScene(pagegoeshere);
+//					stage.setTitle("Borrowed");
+//				} catch (Exception e) {
+//					e.printStackTrace();
+//				}
+//			}
+//      	});
       	
       	backBtn.setOnAction(e-> {
 			
@@ -89,7 +124,6 @@ public class StudentMenu implements AutoCloseable {
       	
       	pane.add(heading, 0, 0);
       	pane.add(searchBtn, 0, 1);
-      	//pane.add(reqIssueBtn, 0, 2);
       	pane.add(viewBorrowedBtn, 0, 2);
       	pane.add(backBtn, 0, 3);
       	
